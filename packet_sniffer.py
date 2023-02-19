74d105d869da5e3b74833a9094ad10741405de56
Aryan <81212535+Aryan4412@users.noreply.github.com>
2023-02-19 23:16:21 +0530

    Update packet_sniffer.py

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,5 +1,4 @@
 #! /usr/local/bin/python3.5
-
 import socket
 import struct
 import textwrap
@@ -91,6 +90,7 @@ def printPacketsV4(filter, data, raw_data):
         src_port, dest_port, length, data = udp_seg(data)
         print('*****UDP Segment*****')
         print('Source Port: {}\nDestination Port: {}\nLength: {}'.format(src_port, dest_port, length))
+
 
 def printPacketsV6(filter, nextProto, newPacket):
     remainingPacket = ""
@@ -185,3 +185,122 @@ def icmpv6Header(data):
 
     data = data[4:]
     return data
+
+
+def nextHeader(ipv6_next_header):
+    if (ipv6_next_header == 6):
+        ipv6_next_header = 'TCP'
+    elif (ipv6_next_header == 17):
+        ipv6_next_header = 'UDP'
+    elif (ipv6_next_header == 43):
+        ipv6_next_header = 'Routing'
+    elif (ipv6_next_header == 1):
+        ipv6_next_header = 'ICMP'
+    elif (ipv6_next_header == 58):
+        ipv6_next_header = 'ICMPv6'
+    elif (ipv6_next_header == 44):
+        ipv6_next_header = 'Fragment'
+    elif (ipv6_next_header == 0):
+        ipv6_next_header = 'HOPOPT'
+    elif (ipv6_next_header == 60):
+        ipv6_next_header = 'Destination'
+    elif (ipv6_next_header == 51):
+        ipv6_next_header = 'Authentication'
+    elif (ipv6_next_header == 50):
+        ipv6_next_header = 'Encapsuling'
+
+    return ipv6_next_header
+
+
+def ipv6Header(data, filter):
+    ipv6_first_word, ipv6_payload_legth, ipv6_next_header, ipv6_hoplimit = struct.unpack(
+        ">IHBB", data[0:8])
+    ipv6_src_ip = socket.inet_ntop(socket.AF_INET6, data[8:24])
+    ipv6_dst_ip = socket.inet_ntop(socket.AF_INET6, data[24:40])
+
+    bin(ipv6_first_word)
+    "{0:b}".format(ipv6_first_word)
+    version = ipv6_first_word >> 28
+    traffic_class = ipv6_first_word >> 16
+    traffic_class = int(traffic_class) & 4095
+    flow_label = int(ipv6_first_word) & 65535
+
+    ipv6_next_header = nextHeader(ipv6_next_header)
+    data = data[40:]
+
+    return data, ipv6_next_header
+
+
+# Unpack Ethernet Frame
+def ethernet_frame(data):
+    proto = ""
+    IpHeader = struct.unpack("!6s6sH",data[0:14])
+    dstMac = binascii.hexlify(IpHeader[0]) 
+    srcMac = binascii.hexlify(IpHeader[1]) 
+    protoType = IpHeader[2] 
+    nextProto = hex(protoType) 
+
+    if (nextProto == '0x800'): 
+        proto = 'IPV4'
+    elif (nextProto == '0x86dd'): 
+        proto = 'IPV6'
+
+    data = data[14:]
+
+    return dstMac, srcMac, proto, data
+
+    # Format MAC Address
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    mac_addr = ':'.join(bytes_str).upper()
+    return mac_addr
+
+# Unpack IPv4 Packets Recieved
+def ipv4_Packet(data):
+    version_header_len = data[0]
+    version = version_header_len >> 4
+    header_len = (version_header_len & 15) * 4
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_len, ttl, proto, ipv4(src), ipv4(target), data[header_len:]
+
+# Returns Formatted IP Address
+def ipv4(addr):
+    return '.'.join(map(str, addr))
+
+
+# Unpacks for any ICMP Packet
+def icmp_packet(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code, checksum, data[4:]
+
+# Unpacks for any TCP Packet
+def tcp_seg(data):
+    (src_port, dest_port, sequence, acknowledgement, offset_reserved_flag) = struct.unpack('! H H L L H', data[:14])
+    offset = (offset_reserved_flag >> 12) * 4
+    flag_urg = (offset_reserved_flag & 32) >> 5
+    flag_ack = (offset_reserved_flag & 32) >> 4
+    flag_psh = (offset_reserved_flag & 32) >> 3
+    flag_rst = (offset_reserved_flag & 32) >> 2
+    flag_syn = (offset_reserved_flag & 32) >> 1
+    flag_fin = (offset_reserved_flag & 32) >> 1
+
+    return src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
+
+
+# Unpacks for any UDP Packet
+def udp_seg(data):
+    src_port, dest_port, size = struct.unpack('! H H 2x H', data[:8])
+    return src_port, dest_port, size, data[8:]
+
+# Formats the output line
+def format_output_line(prefix, string):
+    size=80
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02x}'.format(byte) for byte in string)
+        if size % 2:
+            size-= 1
+            return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
+
+
+main()
